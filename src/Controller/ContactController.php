de08105ccc06a9157070339ca39b9dc9e642ea14
Julian Mican <julian.mican@gmail.com>
2024-04-03 22:26:53 -0500

    Add contact page and form to save in database

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(Request $request): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Process the submitted form data
+            // For example, send an email, save to the database, etc.
+
+            $this->addFlash('success', 'Your message has been sent successfully.');
+
+            return $this->redirectToRoute('contact');
+        }
+
+        return $this->render('contact/contact.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
